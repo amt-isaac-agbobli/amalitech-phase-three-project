bccf21ccd57530112660d66886235ee7e288a9d2
Agbobli Isaac <129859798+agbobliisaac@users.noreply.github.com>
2023-06-09 14:50:11 +0000

    Add users types

---
+++ src/types/user.types.ts
@@ -0,0 +1,16 @@
+export type User = {
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+};
+
+export type UserRead = {
+    id: number;
+    firstName: string;
+    lastName: string;
+    email: string;
+    role : string ;
+    createdAt: Date;
+    updatedAt: Date;
+};
