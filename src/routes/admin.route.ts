8289bc3c1b2972906f055d91cd1d76d6753852a5
Agbobli Isaac <129859798+agbobliisaac@users.noreply.github.com>
2023-06-09 12:47:44 +0000

    Add helper function for hashing password and comparing password

--- src/routes/admin.route.ts
+++ src/routes/admin.route.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import express  from 'express';
+import * as  addminController from '../controllers/admin.controller' ;
 
 const adminRouter : Router = express.Router();
 
+
+adminRouter.post('/register', addminController.registerAdmin); 
 
 adminRouter.get('/', async(req ,res)=>{
     res.status(200).json({
