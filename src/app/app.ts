39991fb2bcb25cdc2dd99cabcdd39c6fe933eb7c
Agbobli Isaac <129859798+agbobliisaac@users.noreply.github.com>
2023-06-09 08:07:49 +0000

    Add script and dumming api for testing

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,7 +4,15 @@ import cors from 'cors' ;
 
 const app : Application = express();
 app.use(cors());
+app.use(express.json()) ;
 
+app.get('/', (req : Request ,res: Response) =>{
+    res.status(200).json({
+        Status : "Success",
+        message : "Development Server"
+    });
+    console.log("I'm Working") ;
+});
 
 
 export default  app ;
