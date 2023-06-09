7c0138ecdfeeccfde6e87406400480cce37ff71f
Agbobli Isaac <129859798+agbobliisaac@users.noreply.github.com>
2023-06-09 12:57:10 +0000

    Fix using single email for duplecate account

--- src/services/admin.service.ts
+++ src/services/admin.service.ts
@@ -1,13 +1,18 @@
 import { db } from "../config/db.server";
-import { Admin ,AdminRead } from "../types/admin.type";
+import * as helper from "../utils/helper";
+import { Admin, AdminRead } from "../types/admin.type";
 
 
-export const registerAdmin = async (admin:Admin) : Promise<Admin> => {
-    const {email ,password} = admin 
-    return await db.admin.create({
-        data:{
-           email,
-           password
-        },
-     })
+export const registerAdmin = async (admin: Admin): Promise<Admin> => {
+   const { email, password } = admin;
+   const adminExist = await db.admin.findUnique({
+      where: { email },
+   });
+   if (adminExist) throw new Error("Admin Already Exist");
+   return await db.admin.create({
+      data: {
+         email,
+         password: await helper.hashPassword(password)
+      },
+   })
 }
--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,6 +1,11 @@
 import bcrypt from 'bcryptjs' ;
 
+
 export const hashPassword = async (password: string) :Promise<string> => {
     const salt = await bcrypt.genSalt(10);
     return await bcrypt.hash(password, salt);
-}
+};
+
+export const comparePassword = async (password: string, hashPassword: string) :Promise<boolean> => {
+    return await bcrypt.compare(password, hashPassword);
+};
