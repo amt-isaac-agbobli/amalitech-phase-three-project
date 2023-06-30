e80d7fc68d26b999743177e537d55853ef1c7d59
Agbobli Isaac <129859798+agbobliisaac@users.noreply.github.com>
2023-06-30 20:50:58 +0000

    Set up ejs

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,16 +6,27 @@ import adminRouter from '../routes/admin.route';
 import userRouter from '../routes/user.route';
 import fileRouter from '../routes/file.route';
 import swaggerUi from 'swagger-ui-express';
+import path from 'path';
 import * as swaggerDoc from '../swagger/swaggerDoc.json'
 
 
 const app: Application = express();
+
+//View engine setup
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "../views"));
 
 
 app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+//app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
 
 //Routes
 app.use('/api/v1/admins/', adminRouter);
